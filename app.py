e34dacca90ad15ec86cce74d0ee59f1072858735
tccmyqp <tccmyqp@gmail.com>
2023-02-07 01:48:20 +0200

    function compile_index_page

--- app.py
+++ app.py
@@ -10,49 +10,52 @@ proxy = {
     # "https": "https://<proxy_host>:<proxy_port>"
 }
 
-form = """
-<form action="http://localhost:5000/form" method="post">
-    <label for="owner_field">owner:</label>
-    <input type="text" name="owner_field" id="owner_field">
-    <label for="repo_field">repo:  </label>
-    <input type="text" name="repo_field" id="repo_field">
-    <input type="submit" value="Изменить">
-</form>
-"""
-
 owner = "tccmyqp"
 repo = "p_test"
-
+                      
 github_link = "https://api.github.com/repos"
 local_link = "http://127.0.0.1:5000/repo"
 
-details_link = f'{local_link}/{owner}/{repo}/details'
-pulls_link = f'{local_link}/{owner}/{repo}/pulls'
-stale_link = f'{local_link}/{owner}/{repo}/pulls/stale'
-issues_link = f'{local_link}/{owner}/{repo}/issues'
-forks_link = f'{local_link}/{owner}/{repo}/forks'
+def compile_index_page(local_link,owner,repo, proxy):
+    change_data_form = """
+                    <form action="http://localhost:5000/form" method="post">
+                        <label for="owner_field">owner:</label>
+                        <input type="text" name="owner_field" id="owner_field">
+                        <label for="repo_field">repo:  </label>
+                        <input type="text" name="repo_field" id="repo_field">
+                        <input type="submit" value="Изменить">
+                    </form>
+                    """
+    
+    details_link = f'{local_link}/{owner}/{repo}/details'
+    pulls_link = f'{local_link}/{owner}/{repo}/pulls'
+    stale_link = f'{local_link}/{owner}/{repo}/pulls/stale'
+    issues_link = f'{local_link}/{owner}/{repo}/issues'
+    forks_link = f'{local_link}/{owner}/{repo}/forks'
 
-index_page = f"""
-<html>
-<body>
-Hello, welcome to the Github REST API proxy!<p>
+    index_page = f"""
+    <html>
+    <body>
+    Hello, welcome to the Github REST API proxy!<p>
 
-using proxy: {proxy["http"]}<p>
+    using proxy: {proxy["http"]}<p>
 
-owner = {owner}, repo = {repo}<p>
+    owner = {owner}, repo = {repo}<p>
 
-{form}<p>
+    {change_data_form}<p>
 
-<a href={details_link}>{details_link}</a><p>
-<a href={pulls_link}>{pulls_link}</a><p>
-<a href={stale_link}>{stale_link}</a><p>
-<a href={issues_link}>{issues_link}</a><p>
-<a href={forks_link}>{forks_link}</a><p>
+    <a href={details_link}>{details_link}</a><p>
+    <a href={pulls_link}>{pulls_link}</a><p>
+    <a href={stale_link}>{stale_link}</a><p>
+    <a href={issues_link}>{issues_link}</a><p>
+    <a href={forks_link}>{forks_link}</a><p>
 
-</body>
-</html>
-"""
+    </body>
+    </html>
+    """
+    return index_page
 
+index_page = compile_index_page(local_link, owner, repo, proxy)
 
 #------------------------------------------------------ логгер
 # Define the logger and set its logging level
@@ -70,51 +73,26 @@ file_handler.setFormatter(formatter)
 # Add the handler to the logger
 logger.addHandler(file_handler)
 
-#------------------------------------------------------ Flask
+#-------------------------------------------------------------- Flask
 logger.info("-------------- app run -----------------")
 
 app = Flask(__name__)
 
+#------------------------------------------------------- отображает индексную станицу
 @app.route("/")
 def index():  
     return index_page
 
+#------------------------------------------------------- получает данные и отображает измененную индексную станицу
 @app.route('/form', methods=['POST'])
 def receive_data():
     global owner, repo, index_page
     owner = request.form['owner_field']
     repo = request.form['repo_field']
-    
-    details_link = f'{local_link}/{owner}/{repo}/details'
-    pulls_link = f'{local_link}/{owner}/{repo}/pulls'
-    stale_link = f'{local_link}/{owner}/{repo}/pulls/stale'
-    issues_link = f'{local_link}/{owner}/{repo}/issues'
-    forks_link = f'{local_link}/{owner}/{repo}/forks'
-    
-    index_page = f"""
-                <html>
-                <body>
-                Hello, welcome to the Github REST API proxy!<p>
-
-                using proxy: {proxy["http"]}<p>
-
-                owner = {owner}, repo = {repo}<p>
-
-                {form}<p>
-
-                <a href={details_link}>{details_link}</a><p>
-                <a href={pulls_link}>{pulls_link}</a><p>
-                <a href={stale_link}>{stale_link}</a><p>
-                <a href={issues_link}>{issues_link}</a><p>
-                <a href={forks_link}>{forks_link}</a><p>
-
-                </body>
-                </html>
-                """
-    
-    # return "Data received: textfield1 = " + text1 + ", textfield2 = " + text2
+    index_page = compile_index_page(local_link, owner, repo, proxy)   
     return index_page
 
+#-------------------------------------------------------/ отображает repo details
 @app.route('/repo/<owner>/<repo>/details', methods=['GET'])
 def repo_details(owner: str, repo: str):
     try:
@@ -126,6 +104,7 @@ def repo_details(owner: str, repo: str):
         logger.error(f'Failed to get repo details from the Github API {owner}/{repo}. Response status code: {response.status_code}')
     
     
+#-------------------------------------------------------/ отображает repo pulls
 @app.route('/repo/<owner>/<repo>/pulls', methods=['GET'])
 def repo_pulls(owner: str, repo: str):
     try:
@@ -137,6 +116,7 @@ def repo_pulls(owner: str, repo: str):
         logger.error(f'Failed to get repo pulls from the Github API {owner}/{repo}. Response status code: {response.status_code}')
             
     
+#-------------------------------------------------------/ отображает pulls stale
 @app.route('/repo/<owner>/<repo>/pulls/stale', methods=['GET'])
 def repo_stale_pulls(owner: str, repo: str):
     try:
@@ -152,6 +132,7 @@ def repo_stale_pulls(owner: str, repo: str):
         logger.error(f'Failed to get repo pulls stale from the Github API {owner}/{repo}. Response status code: {response.status_code}')
 
 
+#-------------------------------------------------------/ отображает repo issues
 @app.route('/repo/<owner>/<repo>/issues', methods=['GET'])
 def repo_issues(owner: str, repo: str):
     try:
@@ -163,6 +144,7 @@ def repo_issues(owner: str, repo: str):
         logger.error(f'Failed to get repo issues stale from the Github API {owner}/{repo}. Response status code: {response.status_code}')
 
 
+#-------------------------------------------------------/ отображает repo forks
 @app.route('/repo/<owner>/<repo>/forks', methods=['GET'])
 def repo_forks(owner: str, repo: str):
     try:
